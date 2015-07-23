e924347efd159a2c435b10a4d163336c96f26c92
ReactiveRaven (David Godfrey) <reactiveraven@gmail.com>
2015-07-23 01:34:19 +0100

    Deduping gitconfig files with include

--- user/_gitconfig.sh
+++ user/_gitconfig.sh
@@ -3,15 +3,12 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
 
 function setGitProfile {
-    read -p "Are you at work? (Y/n): " ANS;
-    case ${ANS:0:1} in
-        n|N)
-            ln -s $DIR/.gitconfig.personal ~/.gitconfig
-            ;;
-        *)
-            ln -s $DIR/.gitconfig.work ~/.gitconfig
-            ;;
-    esac;
+    if hostname | grep "pook" --silent;
+    then
+        ln -s $DIR/.gitconfig.personal ~/.gitconfig
+    else 
+        ln -s $DIR/.gitconfig.work ~/.gitconfig
+    fi;
 }
 
 if [ ! -f ~/.gitconfig ];
